5bdc3daae9298cf79c3277ff6c5de87dd10b81ab
tengjunkai1 <tengjunkai1@jd.com>
2021-12-12 17:41:00 +0800

    master1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    private String catName;
+    private Integer age;
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private Cat cat;
+    private String userName;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
